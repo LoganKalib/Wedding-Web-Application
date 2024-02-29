8dfa54021c2c898f823331faf424d19a8ccf2a9b
LoganKalib <126275811+LoganKalib@users.noreply.github.com>
2024-02-29 14:00:23 +0200

    POJO classes created

--- Reminisce/src/main/java/za/ac/cput/reminisce/Models/Menu.java
+++ Reminisce/src/main/java/za/ac/cput/reminisce/Models/Menu.java
@@ -1,4 +1,48 @@
 package za.ac.cput.reminisce.Models;
 
 public class Menu {
+    private long menuId,eventId;
+    private String type;
+
+    public long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(long menuId) {
+        this.menuId = menuId;
+    }
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Menu() {
+    }
+
+    public Menu(long menuId, long eventId, String type) {
+        this.menuId = menuId;
+        this.eventId = eventId;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuId=" + menuId +
+                ", eventId=" + eventId +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
--- Reminisce/src/main/java/za/ac/cput/reminisce/Models/Organizer.java
+++ Reminisce/src/main/java/za/ac/cput/reminisce/Models/Organizer.java
@@ -1,4 +1,28 @@
 package za.ac.cput.reminisce.Models;
 
 public class Organizer extends Person{
+    private long orgId;
+
+    public long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(long orgId) {
+        this.orgId = orgId;
+    }
+
+    public Organizer() {
+    }
+
+    public Organizer(String name, String surname, String email, String password, long orgId) {
+        super(name, surname, email, password);
+        this.orgId = orgId;
+    }
+
+    @Override
+    public String toString() {
+        return "Organizer{" +
+                "orgId=" + orgId +
+                '}';
+    }
 }
