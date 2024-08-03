6c6821df4115d3db2ab914ede2752f275ac6cb1f
SheldonArendse <sheldonarendse0101@gmail.com>
2024-08-03 12:39:51 +0200

    Removed John and Sarah Modal
    - Temporary fix for duplicate ID reference

--- Reminisce/src/main/resources/static/js/manage-script.js
+++ Reminisce/src/main/resources/static/js/manage-script.js
@@ -1,4 +1,3 @@
-
 function loadContent(section) {
     const sections = document.getElementsByClassName('content-section');
     for (let i = 0; i < sections.length; i++) {
@@ -7,20 +6,36 @@ function loadContent(section) {
     document.getElementById(section + '-content').style.display = 'block';
 }
 
-// Hard coded couple values. Displays teh details for RSVP
+// Hard coded couple values. Displays the details for RSVP
 function showEventDetails(couple) {
-    let modalId = couple === 'John & Sarah' ? 'eventModalJohnSarah' : 'eventModalBarryVanessa';
-    let modal = document.getElementById(modalId);
-    modal.style.display = "block";
-
-    // Close the modal when the user clicks on <span> (x)
-    modal.querySelector('.close').onclick = function() {
-        modal.style.display = "none";
+    let modalId = couple === 'Barry & Vanessa' ? 'eventModalBarryVanessa' : '';
+    if (modalId) {
+        let modal = document.getElementById(modalId);
+        modal.style.display = "block";
     }
+}
 
-    // Close the modal when the user clicks anywhere outside the modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
+// Close the modal when the user clicks on <span> (x)
+window.onload = function() {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        const modal = modals[i];
+        const closeButton = modal.querySelector('.close');
+
+        if (closeButton) {
+            closeButton.onclick = function() {
+                modal.style.display = "none";
+            }
+        }
+    }
+}
+
+// Close the modal when the user clicks anywhere outside the modal
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+        const modal = modals[i];
+        if (event.target === modal) {
             modal.style.display = "none";
         }
     }
